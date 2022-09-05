d4e9cd4f7e9ca8f3d641c31bdf879dbb98e98459
immmmmmmmma <icanasd@gmail.com>
2022-09-05 17:36:00 +0900

    20220905_fullpage

---
+++ 20220830_sass/20220905_fullpage/js/script.js
@@ -0,0 +1,9 @@
+$("#fullpage").fullpage({
+    menu:".navTop",
+    anchors:["m1st","m2nd","m3rd","m4th"],
+    navigation:true,
+    navigationPosition:"fp-right",
+    navigationTooltips:["first","second","third","forth"],
+    responsiveWidth:900
+
+})
