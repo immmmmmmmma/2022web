6b0f7f52b9242dd57cf09b5dc154620f6265fe60
immmmmmmmma <bcisruined@naver.com>
2022-11-18 15:17:34 +0900

    v

--- 20220624_if/js/script2.js
+++ 20220624_if/js/script2.js
@@ -1,9 +1,9 @@
 var userInput = prompt('박,시,현를 입력')
 
-if(userInput != '박' && userInput != '시' && userInput != '현'){
+// if(userInput != '박' && userInput != '시' && userInput != '현'){
 
-}else{
-    var num Math.floor(Math.random()*10)
+// }else{
+//     var num Math.floor(Math.random()*10)
     
 
-}
+// }
